574beadfd921275bff7ac65c82225f22a7a5e957
minsgy <alstjrchl97@naver.com>
2021-08-27 18:47:54 +0900

    feat : App.js Main/sidePanel 생성 및 라우터 설정

---
+++ src/Component/App.js
@@ -0,0 +1,22 @@
+import { initRouter } from "./router.js";
+import MainPage from "./MainPanel/MainPage.js";
+import SidePage from "./SidePanel/SidePage.js";
+
+export default function App({ $target }) {
+  const sidePage = new SidePage({ $target });
+  const mainPage = new MainPage({ $target });
+
+  this.route = () => {
+    // $target.innerHTML = ``;
+    const { pathname } = location;
+    if (pathname === "/") {
+      // sidePage.setState();
+    } else if (pathname.indexOf("/documents/") === 0) {
+      const [, , documentId] = pathname.split("/");
+      console.log(documentId);
+    }
+  };
+
+  this.route();
+  initRouter(() => this.route());
+}
